4dc23ff71805ff35c9c518e13fcf9cbf7cc24227
emmanuelawe <aweemmanuelayobami@gmail.com>
2022-11-22 11:04:50 +0100

    Stories

--- components/Feed.js
+++ components/Feed.js
@@ -4,7 +4,7 @@ import Stories from './Stories'
 const Feed = () => {
   return (
     <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl
-    xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+    xl:grid-cols-3 xl:max-w-5xl mx-auto'>
 
     {/* Section 1 */}
     <section className='col-span-2'>
